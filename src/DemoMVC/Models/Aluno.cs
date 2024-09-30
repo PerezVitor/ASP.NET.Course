5a2bc6217505db7929a92246b84e723382457c00
PerezVitor <vitor.g.perez@hotmail.com>
2024-09-30 19:40:08 -0300

    Imp: Incluindo projeto MVC

---
+++ src/DemoMVC/Models/Aluno.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? DataCadastro { get; set; }
+    public bool Ativo { get; set; }
+}
